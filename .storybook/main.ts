86881d4a3f437b49237ed1d50355138f0171b28d
Humberto López <h.lopez@blackbull.com>
2024-09-19 10:24:33 -0600

    new hero component and static items

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../components/**/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -36,39 +36,39 @@ export default function Typography({ variant, children, color }: TypoProps) {
       Tag = "h1";
       break;
     case "h1":
-      textStyle = "text-xxxl font-bold leading-tight";
+      textStyle = "text-xxxl font-bold leading-none";
       Tag = "h1";
       break;
     case "h2":
-      textStyle = "text-xxl font-bold leading-tight";
+      textStyle = "text-xxl font-bold leading-none";
       Tag = "h2";
       break;
     case "h3":
-      textStyle = "text-xl font-bold leading-tight";
+      textStyle = "text-xl font-bold leading-none";
       Tag = "h3";
       break;
     case "h4":
-      textStyle = "text-lg font-bold leading-tight";
+      textStyle = "text-lg font-bold leading-none";
       Tag = "h4";
       break;
     case "h5":
-      textStyle = "text-base font-bold leading-tight";
+      textStyle = "text-base font-bold leading-none";
       Tag = "h5";
       break;
     case "h6":
-      textStyle = "text-xm font-bold leading-tight";
+      textStyle = "text-xm font-bold leading-none";
       Tag = "h6";
       break;
     case "subheading-lg":
-      textStyle = "text-lg font-semibold uppercase leading-tight";
+      textStyle = "text-lg font-semibold uppercase leading-none";
       Tag = "h4";
       break;
     case "subheading-md":
-      textStyle = "text-md font-semibold uppercase leading-tight";
+      textStyle = "text-md font-semibold uppercase leading-none";
       Tag = "h5";
       break;
     case "subheading-sm":
-      textStyle = "text-sm font-semibold uppercase leading-tight";
+      textStyle = "text-sm font-semibold uppercase leading-none";
       Tag = "h6";
       break;
     case "body":
