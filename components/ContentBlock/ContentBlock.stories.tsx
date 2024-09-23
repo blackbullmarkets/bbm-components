f2097a1c2cd49c991d944a317ba2bfd0b8576b46
Humberto López <h.lopez@blackbull.com>
2024-09-23 11:55:29 -0600

    new content block with image

---
+++ components/ContentBlock/ContentBlock.stories.tsx
@@ -0,0 +1,70 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ContentBlock from "./ContentBlock";
+
+const meta: Meta<typeof ContentBlock> = {
+  component: ContentBlock,
+  argTypes: {
+    featuredImage: {
+      control: "text",
+      description: "An image to display within the block.",
+    },
+    featuredImageWidth: {
+      control: "number",
+      description:
+        "The image width, in pixels. This is required by the <Image /> component.",
+    },
+    featuredImageHeight: {
+      control: "number",
+      description:
+        "Now, the image height, in pixels. This is required by the <Image /> component.",
+    },
+    featuredImageAlt: {
+      control: "text",
+      description:
+        "An alt text for the image. This is required by the <Image /> component.",
+    },
+    heading: {
+      control: "text",
+      description: "A big text to display in this block.",
+    },
+    content: {
+      control: "text",
+      description: "The regular text. The body of the block.",
+    },
+    hasBlockShadow: {
+      control: "boolean",
+      description: "Does this block need a shadow?",
+    },
+    hasBorder: {
+      control: "boolean",
+      description: "Does this block need a border?",
+    },
+    imagePosition: {
+      control: "text",
+      description: "Wether to show the image at the left or right.",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ContentBlock>;
+
+export const Common: Story = {
+  name: "Common",
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    featuredImage: "/images/youtube_thumbnail.png",
+    featuredImageWidth: 640,
+    featuredImageHeight: 358,
+    featuredImageAlt: "How to start trading CFDs?",
+    heading: "Welcome to BlackBull Academy",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    hasBlockShadow: true,
+    hasBorder: true,
+    imagePosition: "left",
+  },
+};
---
+++ components/ContentBlock/ContentBlock.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Typography from "../Typography/Typography";
+
+interface ContentBlockProps {
+  featuredImage: string;
+  featuredImageWidth: number;
+  featuredImageHeight: number;
+  featuredImageAlt: string;
+  heading: string;
+  content: string;
+  hasBlockShadow: boolean;
+  hasBorder: boolean;
+  imagePosition: "left" | "right";
+}
+
+export default function ContentBlock({
+  featuredImage,
+  featuredImageWidth,
+  featuredImageHeight,
+  featuredImageAlt,
+  heading,
+  content,
+  hasBlockShadow,
+  hasBorder,
+  imagePosition,
+}: ContentBlockProps) {
+  return (
+    <section
+      className={`container mx-auto flex gap-6 p-8 
+        ${imagePosition === "left" ? "flex-row" : "flex-row-reverse"}
+        ${hasBlockShadow && "shadow-lg"} 
+        ${hasBorder && "border border-4 border-black-25 rounded-xm"}`}
+    >
+      <div className="basis-1/2">
+        <Image
+          src={featuredImage}
+          width={featuredImageWidth}
+          height={featuredImageHeight}
+          alt={featuredImageAlt}
+        />
+      </div>
+      <div className="flex flex-col gap-6 basis-1/2 justify-center">
+        <Typography variant="h3" color="text-primary-default">
+          {heading}
+        </Typography>
+        <Typography variant="body" color="text-black-50">
+          {content}
+        </Typography>
+      </div>
+    </section>
+  );
+}
