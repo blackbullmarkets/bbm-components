c6fc5b502ab6e7f5b439bd29be57c734c3af83c3
Humberto López <h.lopez@blackbull.com>
2024-10-14 18:59:09 -0600

    adding string type

--- components/TradingPlatformCard/TradingPlatformCard.tsx
+++ components/TradingPlatformCard/TradingPlatformCard.tsx
@@ -6,7 +6,7 @@ interface TradingPlatformCardProps {
   title: string;
   excerpt: string;
   content: string;
-  icon: "tv" | "mt4" | "mt5" | "ctrader" | "bb";
+  icon: "tv" | "mt4" | "mt5" | "ctrader" | "bb" | string;
   link_to: string;
 }
 
